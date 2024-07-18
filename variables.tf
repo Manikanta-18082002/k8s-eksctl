9e17a79903091858883466f8d657c40c22f60986
Mani Kanta <konugantimanikantareddy@gmail.com>
2024-07-18 21:04:06 +0530

    Added some text

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-07ba5f4895acc37f8"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0e273ca043101f2d5"
+}
